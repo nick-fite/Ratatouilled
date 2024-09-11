734d82edf06355b4e488bddd1f7eb985a9f8110c
Fite <nfite@student.uiwtx.edu>
2024-09-11 13:52:24 -0500

    additional work on walk cycle

--- Assets/prefabs/Player/Player.cs
+++ Assets/prefabs/Player/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -16,6 +17,13 @@ public class Player : MonoBehaviour
     private float verticalRot = 0;
     [SerializeField] private Transform TopWalkingPos;
     [SerializeField] private Transform DefaultPos;
+
+    [SerializeField] private float WalkCycleSpeed;
+    private float UpRate;
+    private float DownRate;
+    private float t;
+    private bool isWalking;
+    
 
     void Start()
     {        
@@ -40,14 +48,6 @@ public class Player : MonoBehaviour
 
         _charCont.Move(moveDirection * _speed * Time.deltaTime);
 
-        if(_moveInput != Vector2.zero)
-        {
-            Vector3 test =_armsAnim.gameObject.transform.localPosition;
-            test = Vector3.Lerp(test, TopWalkingPos.localPosition, 1);
-            _armsAnim.gameObject.transform.SetLocalPositionAndRotation(test, _armsAnim.gameObject.transform.localRotation);
-
-        }
-
         // Camera controls
         float mouseX = Input.GetAxis("Mouse X") * _LookSensX;
         float mouseY = Input.GetAxis("Mouse Y") * _LookSensY;
@@ -57,6 +57,24 @@ public class Player : MonoBehaviour
 
         _camera.transform.localRotation = Quaternion.Euler(verticalRot, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+
+        if(isWalking){
+        Vector3 target;
+        if(_armsAnim.gameObject.transform.localPosition == TopWalkingPos.localPosition)
+        {
+            target = DefaultPos.localPosition;
+            t += Time.deltaTime * DownRate;
+        }
+        else 
+        {
+            target = TopWalkingPos.localPosition;
+            t += Time.deltaTime * UpRate;  
+        }
+        Vector3 newPos = _armsAnim.gameObject.transform.localPosition;
+        newPos  = Vector3.Lerp(newPos, target, t);
+
+        _armsAnim.gameObject.transform.SetLocalPositionAndRotation(newPos, _armsAnim.gameObject.transform.localRotation);
+        }
     }
     
     void Punching()
@@ -66,13 +84,37 @@ public class Player : MonoBehaviour
             _punching = 0;
     }
 
+
+
+/*
+if(_moveInput != Vector2.zero)
+        {
+            Vector3 test =_armsAnim.gameObject.transform.localPosition;
+            test = Vector3.Lerp(test, TopWalkingPos.localPosition, 1);
+            _armsAnim.gameObject.transform.SetLocalPositionAndRotation(test, _armsAnim.gameObject.transform.localRotation);
+
+        }
+*/
+
     public void OnWalk(InputValue value)
     {
+        UpRate = 1.0f/ Vector3.Distance(TopWalkingPos.localPosition, _armsAnim.transform.localPosition) * WalkCycleSpeed;
+        DownRate = 1.0f/ Vector3.Distance(DefaultPos.localPosition, TopWalkingPos.localPosition) * WalkCycleSpeed;
         _moveInput = value.Get<Vector2>();
+        isWalking = true;
+        if(_moveInput == Vector2.zero)
+        {
+            isWalking = false;
+        }
     }
 
     public void OnPunch(InputValue value)
     {
         _punching = value.Get<float>();
+    }
+
+    public IEnumerator WalkCycle()
+    {
+        yield return null;
     }
 }
