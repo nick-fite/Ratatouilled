250b0e0bc5f237e082db4bb69c1609cb6b381114
Fite <nfite@student.uiwtx.edu>
2024-09-11 11:41:52 -0500

    started adding walk cycle, changed input method

--- Assets/prefabs/Player/Player.cs
+++ Assets/prefabs/Player/Player.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -9,11 +10,12 @@ public class Player : MonoBehaviour
     private Camera _camera;
     private PlayerInput _playerInput;
     private CharacterController _charCont;
-    private Animator _armsAnim;
+    [SerializeField] private Animator _armsAnim;
     private Vector2 _moveInput;
     private float _punching;
-    
     private float verticalRot = 0;
+    [SerializeField] private Transform TopWalkingPos;
+    [SerializeField] private Transform DefaultPos;
 
     void Start()
     {        
@@ -21,18 +23,32 @@ public class Player : MonoBehaviour
         _charCont = GetComponent<CharacterController>();
         _camera = GetComponentInChildren<Camera>();
         _armsAnim = GetComponentInChildren<Animator>();
-
+        
         Cursor.lockState = CursorLockMode.Locked;
         _playerInput.Player.Enable();        
     }
-    
-    void Update()
+
+    void FixedUpdate()
     {
-        _moveInput = _playerInput.Player.walk.ReadValue<Vector2>();
+        Movement();
+        Punching();
+    }
+
+    void Movement()
+    {
         Vector3 moveDirection = transform.forward * _moveInput.y + transform.right * _moveInput.x; 
 
         _charCont.Move(moveDirection * _speed * Time.deltaTime);
 
+        if(_moveInput != Vector2.zero)
+        {
+            Vector3 test =_armsAnim.gameObject.transform.localPosition;
+            test = Vector3.Lerp(test, TopWalkingPos.localPosition, 1);
+            _armsAnim.gameObject.transform.SetLocalPositionAndRotation(test, _armsAnim.gameObject.transform.localRotation);
+
+        }
+
+        // Camera controls
         float mouseX = Input.GetAxis("Mouse X") * _LookSensX;
         float mouseY = Input.GetAxis("Mouse Y") * _LookSensY;
         
@@ -41,12 +57,22 @@ public class Player : MonoBehaviour
 
         _camera.transform.localRotation = Quaternion.Euler(verticalRot, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
-        
-        _punching = _playerInput.Player.punch.ReadValue<float>();
-
+    }
+    
+    void Punching()
+    {
         if(_punching > 0)
-            _armsAnim.SetBool("Punching", true);
-        else 
-            _armsAnim.SetBool("Punching", false);
+            _armsAnim.SetTrigger("Punching");
+            _punching = 0;
+    }
+
+    public void OnWalk(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+
+    public void OnPunch(InputValue value)
+    {
+        _punching = value.Get<float>();
     }
 }
